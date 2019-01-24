e2787a4e5b7c381c70b9f97a4f8ad3318dbc1e99
Peter Ramos <peterdramos02@gmail.com>
2019-01-24 15:58:14 -0500

    move form to the proper place

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import './App.css';
 import Input from './components/input';
 import Output from './components/output';
+import Button from './components/button';
 import Image from './components/images';
 import China from './assets/flags/china.png';
 import UnitedStates from './assets/flags/united-states.png';
@@ -24,8 +25,11 @@ class App extends Component {
 
     return (
       <div className="App">
+      <form onSubmit={(e) => e.preventDefault()}className="App">
         <Input name="country" placeholder="Country" handleInput={(e) => this.handleInput(e)}/>
         <Input name="year" placeholder="Year" handleInput={(e) => this.handleInput(e)}/>
+        <Button />
+      </form>
         <Image imgSrc={UnitedStates} />
         <Output country={this.props.countryData} year={2000} />
       </div>
--- src/components/input.js
+++ src/components/input.js
@@ -1,12 +1,8 @@
 import React from 'react';
-import Button from './button';
 
 function Input(props) {
   return (
-    <form onSubmit={(e) => e.preventDefault()}className="App">
-      <input name={props.name} onChange={e => props.handleInput(e)} type="text" placeholder={props.placeholder} />
-      <Button />
-    </form>
+    <input name={props.name} onChange={e => props.handleInput(e)} type="text" placeholder={props.placeholder} />
   );
 }
 
