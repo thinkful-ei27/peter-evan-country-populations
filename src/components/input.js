769014b596bb813c478fb51b6bae337e1ae2ee1e
Peter Ramos <peterdramos02@gmail.com>
2019-01-24 12:43:40 -0500

    add some boilerplate for app and input components

---
+++ src/components/input.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+export default function Input (props)  {
+
+    return (
+      <form className="App">
+        <input
+          type="text"
+          placeholder="China, India or United States"
+        ></input>
+      </form>
+    );
+}
